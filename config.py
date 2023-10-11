75fcfd6930ec27ac92ab0bc6a0572b724cb118c8
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-10-11 22:49:05 +0500

    minor changes

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DbSettings(BaseModel):
-    alembic_url: str = f"postgresql+asyncpg://postgres:77girado@bikeland.uz:5433/postgres"
+    alembic_url: str = f"postgresql+asyncpg://postgres:77girado@api.bikeland.uz:5433/postgres"
         
 
 
@@ -33,7 +33,7 @@ class Settings(BaseSettings):
 
     @property
     def DATABASE_URL(self): 
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
 
     media_filename: str = "media"
