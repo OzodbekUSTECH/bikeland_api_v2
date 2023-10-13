1d84ff0989f087dc0eb4ffaf3b0bdce31ba0972f
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-10-13 05:17:02 +0500

    reqs

--- database/db.py
+++ database/db.py
@@ -2,6 +2,6 @@ from config import settings
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 engine = create_async_engine(settings.DATABASE_URL, echo=settings.ECHO, pool_size=10, max_overflow=20)
-session_maker = async_sessionmaker(engine,expire_on_commit=False) # autoflush=False, autocommit=False, 
+session_maker = async_sessionmaker(engine,autoflush=False, autocommit=False, expire_on_commit=False) # 
  
 
