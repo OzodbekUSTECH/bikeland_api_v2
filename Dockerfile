f546e831290b900ddf2ae36c76461ec0ffec9b58
OzodbekUSTECH <naimovozod81@gmail.com>
2024-02-16 19:33:12 +0500

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,13 @@
 # pull the official docker image
 FROM python:3.13.0a3-slim
-
 # Set the timezone to Asia/Tashkent
 ENV TZ=Asia/Tashkent
-
 # set work directory
 WORKDIR /project
-
 # set env variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-# Install system dependencies
-RUN apt-get update && \
-    apt-get install -y \
-    build-essential \
-    libpq-dev \
-    libffi-dev \
-    libgreenlet-dev \
-    curl \
-    && rm -rf /var/lib/apt/lists/*
-
-# Install Rust
-RUN curl https://sh.rustup.rs -sSf | sh -s -- -y
-ENV PATH="/root/.cargo/bin:${PATH}"
-
-# Install Python dependencies
 COPY requirements.txt .
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
@@ -33,13 +15,12 @@ RUN pip install -r requirements.txt
 # copy project
 COPY . .
 
-# Install NGINX
+
 RUN apt-get update && \
     apt-get install -y nginx && \
     rm -rf /etc/nginx/sites-enabled/default
-COPY nginx.conf /etc/nginx/sites-enabled/
-
+	@@ -29,4 +39,4 @@ COPY nginx.conf /etc/nginx/sites-enabled/
 EXPOSE 80
 
 # Запускаем Nginx и Gunicorn для приложения FastAPI
-CMD service nginx start && alembic upgrade head && gunicorn -b 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker main:app
+CMD service nginx start && alembic upgrade head && gunicorn -b 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker main:app 
--- test.py
+++ test.py
@@ -79,9 +79,56 @@
 # # if __name__ == "__main__":
 # #     asyncio.run(main())
 
-from services import products_service
-import asyncio
-from utils.parser import ParserHandler
+# from services import products_service
+# import asyncio
+# from utils.parser import ParserHandler
 
-# asyncio.run(ParserHandler.get_by_key("5P6000009"))
-asyncio.run(products_service.create_products())
+# # asyncio.run(ParserHandler.get_by_key("5P6000009"))
+# asyncio.run(products_service.create_products())
+
+
+# # pull the official docker image
+# FROM python:3.13.0a3-slim
+
+# # Set the timezone to Asia/Tashkent
+# ENV TZ=Asia/Tashkent
+
+# # set work directory
+# WORKDIR /project
+
+# # set env variables
+# ENV PYTHONDONTWRITEBYTECODE 1
+# ENV PYTHONUNBUFFERED 1
+
+# # Install system dependencies
+# RUN apt-get update && \
+#     apt-get install -y \
+#     build-essential \
+#     libpq-dev \
+#     libffi-dev \
+#     libgreenlet-dev \
+#     curl \
+#     && rm -rf /var/lib/apt/lists/*
+
+# # Install Rust
+# RUN curl https://sh.rustup.rs -sSf | sh -s -- -y
+# ENV PATH="/root/.cargo/bin:${PATH}"
+
+# # Install Python dependencies
+# COPY requirements.txt .
+# RUN pip install --upgrade pip
+# RUN pip install -r requirements.txt
+
+# # copy project
+# COPY . .
+
+# # Install NGINX
+# RUN apt-get update && \
+#     apt-get install -y nginx && \
+#     rm -rf /etc/nginx/sites-enabled/default
+# COPY nginx.conf /etc/nginx/sites-enabled/
+
+# EXPOSE 80
+
+# # Запускаем Nginx и Gunicorn для приложения FastAPI
+# CMD service nginx start && alembic upgrade head && gunicorn -b 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker main:app
