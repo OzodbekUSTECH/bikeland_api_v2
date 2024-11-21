cf19a656275d9f07bd6a2ea73b4f0d6261331583
OzodbekUSTECH <naimovozod81@gmail.com>
2024-11-21 20:37:16 +0300

    updated dbsettings url

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DbSettings(BaseModel):
-    alembic_url: str = f"postgresql+asyncpg://postgres:77girado@185.200.243.110:5433/postgres"
+    alembic_url: str = f"postgresql+asyncpg://postgres:77girado@188.225.58.20:5433/postgres"
         
 
 
